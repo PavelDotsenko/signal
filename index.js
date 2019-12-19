e04e974117437e5e1227f022439ca8b304c6032f
Dmitriy Shatalov <deman4ik@gmail.com>
2019-12-19 20:42:56 +0300

    feat: load font, prettier reformat

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const { generateSignalImage } = require('./chartGenerator');
+const { generateSignalImage } = require("./chartGenerator");
 
 const robot = {
-    "id": "37dd7d07-99f3-471c-997d-cae0cbe188eb",
-    "name": "PAR-1 Bitfinex XLM/USD 8h",
-    "exchange": "bitfinex",
-    "asset": "XLM",
-    "currency": "USD",
-    "timeframe": 480
+  id: "37dd7d07-99f3-471c-997d-cae0cbe188eb",
+  name: "PAR-1 Bitfinex XLM/USD 8h",
+  exchange: "bitfinex",
+  asset: "XLM",
+  currency: "USD",
+  timeframe: 480
 };
 
 const signal = {
-    "code": "p_15",
-    "price": 0.057316,
-    "action": "closeShort",
-    "orderType": "stop",
-    "timestamp": "2019-12-16T08:00:01.422Z",
-    "candleTimestamp": "2019-12-16T00:00:00.000Z"
+  code: "p_15",
+  price: 0.057316,
+  action: "closeShort",
+  orderType: "stop",
+  timestamp: "2019-12-16T08:00:01.422Z",
+  candleTimestamp: "2019-12-16T00:00:00.000Z"
 };
 
 generateSignalImage(robot, signal);
